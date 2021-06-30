d3f30ebd9537fd7d316479633791be9229c2465e
Cynthia2019 <ccynthia2019@gmail.com>
2021-07-01 00:51:29 +0800

    sliding window

--- silding_window.py
+++ silding_window.py
@@ -3,9 +3,10 @@
 1. 先设left = right = 0，slideWindow = [left, right]
 2. 先扩大right的值，使得slideWindow内的元素满足要求
 3. 扩大left的值，直到slideWindow内的元素不满足要求；每一次扩大left前，要更新一边当前最优解
-4. repeat 2 和 3，直到right == len(s) - 1s
+4. repeat 2 和 3，直到right == len(s) 
 ***2找到当前最优解，3优化最优解，最后得到全局最优解
 """
+#3.Longest Substring Without Repeating Characters
 def lengthOfLongestSubstring(self, s: str) -> int:
     #滑动窗口
     left = 0 
@@ -22,4 +23,21 @@ def lengthOfLongestSubstring(self, s: str) -> int:
             right = left
         max_len = max(max_len, len(window))
     return max_len
-            
+
+#121. Best Time to Buy and Sell Stock
+#O(n)
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 0: 
+            return 0
+        #滑动窗口
+        left = 0 
+        right = 1
+        max_profit = 0
+        while right != len(prices): 
+            if prices[right] - prices[left] > 0: #右移条件
+                max_profit = max(prices[right] - prices[left], max_profit)
+                right += 1
+            else: 
+                left += 1
+                right = left + 1
+        return max_profit
